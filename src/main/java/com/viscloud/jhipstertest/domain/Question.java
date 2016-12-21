a5c4c7af956b9c9adbf876cbde9012d23343c8c7
Antonin Slezacek <slezicz@gmail.com>
2016-12-21 17:46:35 +0100

    add Question table

---
+++ src/main/java/com/viscloud/jhipstertest/domain/Question.java
@@ -0,0 +1,72 @@
+package com.viscloud.jhipstertest.domain;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Question.
+ */
+@Entity
+@Table(name = "question")
+public class Question implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Question content(String content) {
+        this.content = content;
+        return this;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Question question = (Question) o;
+        if (question.id == null || id == null) {
+            return false;
+        }
+        return Objects.equals(id, question.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+            "id=" + id +
+            ", content='" + content + "'" +
+            '}';
+    }
+}
